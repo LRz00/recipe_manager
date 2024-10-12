22a98995000359bb67e77001b5bb7e2f5855e73d
Lara Rodrigues <lararods73@gmail.com>
2024-10-12 12:23:04 -0300

    finished recipe manager functions

--- manager.rs
+++ manager.rs
@@ -33,7 +33,41 @@ impl RecipeManager {
         self.recipes.iter().find(|r| r.id == id)
     }
 
-    pub fn update_recipe(&mut self, name: String, ingredients: Vec<String>, instructions: Vec<String>, servings: u32) -> bool{
-        //to implement
+    pub fn update_recipe(&mut self, id: i32 ,name: String, ingredients: Vec<String>, instructions: Vec<String>, servings: u32) -> bool{
+        if let Some(recipe) = self.recipes.iter_mut().find(|r| r.id == id){
+            recipe.name = name;
+            recipe.ingredients = ingredients;
+            recipe.instructions = instructions;
+            recipe.servings = servings;
+            
+            true
+        }
+        else{
+            false
+        }
+    }
+
+    pub fn delete_recipe(&mut self, id: i32) -> bool{
+        let initial_len = self.recipes.len();
+
+        self.recipes.retain(|r| r.id != id);
+
+        self.recipes.len() < initial_len
+    }
+
+    pub fn save_to_file(&self, filename: &str) -> std::io::Result<()>{
+        let json = serde_json::to_string(&self.recipes)?;
+
+        fs::write(filename, json);
+    }
+
+    pub fn load_from_file(&mut self, filename: &str) -> std::io::Result<()>{
+        let json = fs::read_to_string(filename)?;
+
+        self.recipes = serde_json::from_str(&json)?;
+
+        self.next_id = self.recipes.iter().map(|r| r.id).max().unwrap_or(0) + 1;
+
+        Ok(())
     }
 }
